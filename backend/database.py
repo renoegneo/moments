f9ffb6b5b562c8b2a915ae25a2ab2dfe4895b826
Maxut <micmac337@mail.ru>
2025-11-01 16:16:17 +0600

    фикс трех букв

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import DeclarativeBase
 from core.config import settings
 
-engine = create_async_engine(settings.database_url, echo=True, future=True)
+engine = create_async_engine(settings.DB_HOST, echo=True, future=True)
 
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
