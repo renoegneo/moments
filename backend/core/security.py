5de3bd857d66c202a7bc871f9f6da82f5af2df93
Maxut <micmac337@mail.ru>
2025-11-07 23:36:18 +0600

    fixed verify pswd, deleted passlib

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,9 +1,28 @@
-from passlib.context import CryptContext
+import bcrypt
+from datetime import timedelta, datetime, timezone
+from jose import JWTError, jwt
+from .config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+def hash_password(pswd: str) -> str:
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(pswd.encode('utf-8'), salt)
+    # тупые пиндосы сделали бкрипт битовым, надо кодировку указывать
+    return hashed.decode('utf-8')
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+def verify_password(plain_pswd: str, hashed_pswd: str) -> bool:
+    return bcrypt.checkpw(plain_pswd.encode('utf-8'), hashed_pswd.encode('utf-8'))
+
+def create_token(data: dict, expires_delta: timedelta) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
+
+def decode_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        return payload
+    except JWTError:
+        raise ValueError("Невалидный токен")
